e79fa34385faabc12add6f32d423a45644332ace
Zarina <zarinamay7@gmail.com>
2023-02-18 21:38:45 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# a = int(input('enter the number: '))
+# b = int(input('enter the number:'))
+# c = int(input('enter the number:'))
+# print(a+b+c)
+
+# a = int(input('enter the number: '))
+# b = int(input('enter the number:'))
+# if (a % 2 == 0 and b % 2 == 0) or (a % 2 == 1 and b % 2 == 1):
+#     print('same')
+# else :print('not same')
+
